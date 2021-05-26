ebf1c2203b0f93a731eaa9343d12fbbfb2e7c1fd
GrVl-dev <vlgrv80@gmail.com>
2021-05-26 22:42:41 +0400

    1.6 completed

--- SBER/Контесты/1.6.py
+++ SBER/Контесты/1.6.py
@@ -1,3 +1,22 @@
+def max_list(lst):
+    new_list = []
+    val = []
+    dct = {}
+    res = ''
+    for j in lst:
+        if j not in dct:
+            dct[j] = lst.count(j)
+            val.append(lst.count(j))
+    val.sort(reverse=True)
+    for k, v in dct.items():
+        if v == val[0]:
+            new_list.append(k)
+    new_list.sort()
+    for n in new_list:
+        res += str(n) + ' '
+    return res
+
+
 data_input = []
 data_output = ''
 flight_ids = []
@@ -7,7 +26,8 @@ num = int(input())
 for i in range(num):
     data_input.append(input().split(','))
     flight_id = data_input[i][1]
-    flight_ids.append(flight_id)
+    flight_ids.append(int(flight_id))
 
-print(flight_ids)
-
+result = max_list(flight_ids)
+result = result.strip()
+print(result)
